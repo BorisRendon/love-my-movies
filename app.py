638d7b9c769710cf0b91a10901d68ef14715295c
BorisRendon <boris.rendon@ufm.edu>
2019-10-20 21:20:32 -0600

    Avance en peliculas destacadas

--- app.py
+++ app.py
@@ -6,33 +6,112 @@ import requests
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 
-#api_key = 0f90dc240d06fa41a1a68cbc6abd44af
+
+#titulo, release date, popularidad , link a mas información
+
+
+app = Flask(__name__)
+
+
+
 
 tmdb = TMDb()
 tmdb.api_key = '0f90dc240d06fa41a1a68cbc6abd44af'
 
 movie = Movie()
 popular_pelis = movie.popular()
-for p in popular_pelis:
-    print(p.title)
-    #print(p.overview)
 
-developer = os.getenv("DEVELOPER", "Me")
-environment=os.getenv("ENVIRONMENT","development")
+
+#resultado de todas las peliculas famosas, son 14
+for p in popular_pelis:
+    titulos = str(p.title)
+    release_date = str(p.release_date)
+    popularity = str(p.popularity)
+    #overview = str(p.overview)
+    
+    #votos= str(p.vote_count)
+
+
+    titulos_lista = titulos.split("\n")
+    #overview_lista = overview.split("\n")
+    release_date_lista = release_date.split("\n")
+    #votos_lista = votos.split("n")
+
+    w = (len(titulos))
+    #print(titulos + ":\n=>" + overview + "\n" + "Fecha de lanzamiento: " + release_date + "\n Total de votos: "+ votos)
+
+#print("Titulo : " + titulos_lista[0] +"\nOverview: "+ overview_lista[0]+"\nFecha de lanzamiento: " + release_date_lista[0]+"\nVotos: " + votos_lista[0])
+
+
+#top 5 movies
+#joker---------------------------
+jok = movie.details(475557)
+joker_title = jok.title
+#joker_overview = jok.overview
+joker_release_date =str(jok.release_date)
+#joker_votos = str(jok.vote_count)   
+joker_popularity = str(jok.popularity)
+#-----------------------------------------
+
+#avengers
+avengers = movie.search('Avengers: Infinity War')
+for av in avengers:
+    avengers_titulo = av.title
+    avengers_release_date = str(av.release_date)
+    avengers_popularity = str(av.popularity)
+   
+    
+    #avengers_votos = str(av.vote_count)
+    #avengers_overview = av.overview
+
+#dora
+dora = movie.search('Dora and the Lost City of Gold')
+for dor in dora:
+    dora_title = dor.title
+    dora_release_date = str(dor.release_date)
+    dora_popularity = str(dor.popularity)
+    
+    
+    #dora_votos = (dor.vote_count)
+    #dora_overview = dor.overview
+
+
+#deadpool2
+deadpool2 = movie.details(383498)
+deadpool_title = deadpool2.title
+deadpool_release_date =str(deadpool2.release_date)
+deadpool_popularity = deadpool2.popularity
+#deadpool_votos = str(deadpool2.vote_count)
+#deadpool_overview = deadpool2.overview
+
+#pulp fiction
+pulp_fiction = movie.details(680)
+pulp_title = pulp_fiction.title
+pulp_release_date = str(pulp_fiction.release_date)
+pulp_popularity = str(pulp_fiction.popularity)
+
+ 
+
+
+
+
+
+
+
 #todas las peliculas
-peliculas = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=0f90dc240d06fa41a1a68cbc6abd44af&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-peliculas_json = peliculas.json()
+#peliculas = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=0f90dc240d06fa41a1a68cbc6abd44af&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+#peliculas_json = peliculas.json()
 #print(peliculas_json)
 
 #muestra todos los elementos que quiero de las peliculas
-for pelis in peliculas_json['results']:
+#for pelis in peliculas_json['results']:
 
-    titulos_pelis = (pelis['title'])
-    votos_pelis = str((pelis['vote_count']))
-    release_pelis =str((pelis['release_date']))
+#    titulos_pelis = (pelis['title'])
+ #   votos_pelis = str((pelis['vote_count']))
+  #  release_pelis =str((pelis['release_date']))
     #print(titulos_pelis+ " \ncantidad de votos: =>" + votos_pelis+ "\n fecha de lanzamiento :" +release_pelis)
 
-    y=(len(pelis))
+   # y=(len(pelis))
  
 
 #print(y)
@@ -47,24 +126,16 @@ shawshank_movie_json = shawshank_movie.json()
 
 
 
-with open('joker_movie_details.json' , 'r') as myfile:
-    data = myfile.read()
+@app.route('/')
+def index():
 
-#parsear file
-obj = json.loads(data)
-joker_title = (str(obj['original_title']))
-
-
-
-
-
-app = Flask(__name__)
-#@app.route('/')
-#def index():
     
- #   return render_template('layout.html'  , y=y,titulos_pelis=titulos_pelis,peliculas_json=peliculas_json)
-#if __name__ == '__main__':
- #   app.run(debug=True)
+    return render_template('layout.html'  , w=w,titulos=titulos,popular_pelis = popular_pelis )
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
+
 
 
 #class Movies():
